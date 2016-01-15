4ba21629e0cd966a43948ecb6ec0bc8db3318fac
duandi <heroduandi@gmail.com>
2016-01-15 14:45:27 +0800

    add responsive

--- js/scripts.js
+++ js/scripts.js
@@ -37,16 +37,16 @@ $(document).ready(function(){
     function autoplay(){
         play=setInterval(function(){
             index++
-            if(index>1){index=-1;}else{
-                $(".news_title").children().eq(index).animate({
-                    opacity:1
-                },200);
-                $(".news_title").children().eq(index).siblings().animate({
-                    opacity:0.7
-                },200);
-                $(".news_main ul").eq(index).siblings().hide();
-                $(".news_main ul").eq(index).fadeIn();
-            }
+            if(index>1){index=0;}
+            $(".news_title").children().eq(index).animate({
+                opacity:1
+            },200);
+            $(".news_title").children().eq(index).siblings().animate({
+                opacity:0.7
+            },200);
+            $(".news_main ul").eq(index).siblings().hide();
+            $(".news_main ul").eq(index).fadeIn();
+
         },2000)
     }
     autoplay();
